e949e6d4d2f6d6abe93759f57ebe5c3085c210a4
unknown <kokwai2107@gmail.com>
2023-10-11 13:45:13 +0800

    Added Date test codes with output stream

--- DateTest.cpp
+++ DateTest.cpp
@@ -1,5 +1,6 @@
 
 #include<iostream>
+#include<fstream>
 
 #include "Date.h"
 #include "Utils.h"
@@ -8,26 +9,104 @@ using namespace std;
 
 int main()
 {
+    const string INFILE("Tests/DateTest.txt");
     Utils utils;
-    Date date(21, 7, 2020);
+    Date date;
+    ofstream ofs(INFILE);
+    Date paramDate(21,7,2020);
+
+    if(!ofs)
+    {
+        return -1;
+    }
+
+    cout << "Date Test File: \n====Test1: Test on default constructor with initial value to be set as 0====" << endl;
+    ofs << "Date Test File: \n====Test1: Test on default constructor with initial value to be set as 0====" << endl;
 
     utils.Assert(
-        date.GetDay() == 21,
-        "Date has been set as 21",
-        "Date should be set as 21"
+        date.GetDay() == 0,
+        "Date has been set as 0",
+        "Date should be set as 0",
+        ofs
     );
 
     utils.Assert(
-        date.GetMonth() == 7,
-        "Date has been set as 7",
-        "Date should be set as 7"
+        date.GetMonth() == 0,
+        "Date has been set as 0",
+        "Date should be set as 0",
+        ofs
     );
 
     utils.Assert(
-        date.GetYear() == 2020,
-        "Date has been set as 2020",
-        "Date should be set as 2020"
+        date.GetYear() == 0,
+        "Date has been set as 0",
+        "Date should be set as 0",
+        ofs
     );
+
+    cout << '\n' << "====Test2: Test on Parameterized constructor with user defined value==== \n";
+    ofs << '\n' << "====Test2: Test on Parameterized constructor with user defined value==== \n";
+
+
+    utils.Assert(
+        paramDate.GetDay() == 21,
+        "Parameterized constructor created with day set as 21",
+        "Parameterized constructor should be constructed with day set as 21",
+        ofs
+    );
+
+    utils.Assert(
+        paramDate.GetMonth() == 7,
+        "Parameterized constructor created with day set as 7",
+        "Parameterized constructor should be constructed with day set as 7", ofs
+    );
+
+    utils.Assert(
+        paramDate.GetYear() == 2020,
+        "Parameterized constructor created with day set as 2020",
+        "Parameterized constructor should be constructed with day set as 2020", ofs
+    );
+
+    cout << '\n' << "====Test3: Test on Date Class Setters in setting user defined values==== \n";
+    ofs << '\n' << "====Test3: Test on Date Class Setters in setting user defined values==== \n";
+    unsigned day = 1;
+    unsigned month = 1;
+    unsigned year = 2000;
+    cout << "Test Value: --->" << "day: " << day << ", month: " << month << ", year: " << year << endl;
+    ofs << "Test Value: --->" << "day: " << day << ", month: " << month << ", year: " << year << endl;
+
+    paramDate.SetDay(day);
+    paramDate.SetMonth(month);
+    paramDate.SetYear(year);
+    utils.Assert(
+        paramDate.GetDay() == day,
+        "Set day to 1 successfully",
+        "Date day should be set to 1", ofs
+    );
+
+    utils.Assert(
+        paramDate.GetMonth() == month,
+        "Set month to 1 successfully",
+        "Date month should be set to 1", ofs
+    );
+
+    utils.Assert(
+        paramDate.GetYear() == year,
+        "Set year to 2000 successfully",
+        "Date year should be set to 2000", ofs
+    );
+
+    cout << '\n' << "====Test4: Test on output stream of date class==== \n";
+    cout << "Test Value: --->" << "day: " << day << ", month: " << month << ", year: " << year << endl;
+    cout << "Actual cout display:" << endl;
+    cout << paramDate;
+
+    ofs << '\n' << "====Test4: Test on output stream of date class==== \n";
+    ofs << "Test Value: --->" << "day: " << day << ", month: " << month << ", year: " << year << endl;
+    ofs << "Actual cout display:" << endl;
+    ofs << paramDate;
+
+    ofs.close();
 
     return 0;
 }
--- Utils.h
+++ Utils.h
@@ -1,23 +1,47 @@
+/// @file Utils.h
+/// @brief Utils Class that hold helper functions.
+///
+/// This class contains the assertion helper function.
+///
+/// @author Chan Kok Wai
+///
+///
+
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
 #include<iostream>
 #include<string>
+#include<fstream>
 
 using namespace std;
 
 class Utils
 {
 public:
-    void Assert(bool condition, string passMessage, string failMessage)
+
+    ///
+    /// @brief Method function
+    ///
+    /// @details This is a function used for running test.
+    ///
+    /// @param condition This is where the condition of assertion will be set as
+    /// @param passMessage This contains the pass Message
+    /// @param failMessage This contains the fail Message
+    /// @param ofs This is used for passing in the output stream
+    ///
+    void Assert(bool condition, string passMessage, string failMessage, ostream &ofs)
     {
         if(condition)
         {
             cout << "[PASS]: " << passMessage << endl;
+            ofs << "[PASS]: " << passMessage << endl;
+
         }
         else
         {
             cout << "[FAIL]: " << failMessage << endl;
+            ofs << "[FAIL]: " << failMessage << endl;
         }
     }
 };
